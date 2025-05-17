7c1b87a9135b444fa680866ccce5adf2f2051886
Gourav Kumar Mallick <12b.39gourav@gmail.com>
2025-05-17 14:01:05 +0000

    GNU Assembler directory formed.

--- gnu_assembler/add_two.s
+++ gnu_assembler/add_two.s
@@ -1,41 +1,30 @@
+# Data declaration
+
 	.section .data
 a: .long 8
 b: .long 12
-c: .long 0
+res: .space 10		# 10 bytes free space
 
-res: .space 10		# Allocating 10 bytes.
-ptr: .long 0
-newline: .ascii "\n"
 
+
+# Main function
 
 	.section .text
 	.globl _start
 
-_start:
-	movl $a, %eax
-	movl $b, %ebx
-	addl %ebx, %eax
-	movl %eax, c
 
-	movl res, $ptr
+_start:
+	
+	# Loading both numbers.
+
+	movl a, %eax
+	movl b, %ebx
+
+
+	# Adding both numbers.
+
+	addl %ebx, %eax
+
 
 	to_ascii:
-		divl $10, $c 		# Doubtful
-		addl 0x30, %eax
-		addl $1, ptr
-		movl %eax, ptr
-
-		cmpl $0, $c
-		jz to_ascii
-
-	movl $newline, ptr
-
-	movl $1, %eax
-	movl $1, %ebx
-	movl $res, %ecx
-	movl $4, %edx
-	int 0x80
-
-	movl $1, %eax
-	movl $0, %ebx
-	int 0x80
+		
